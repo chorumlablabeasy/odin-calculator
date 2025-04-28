1e4dafad4938b16702124a5049a7278d9f74a996
mami <115097751+chorumlablabeasy@users.noreply.github.com>
2025-04-28 18:07:15 +0300

    create all files and write basic math functions

---
+++ script.js
@@ -0,0 +1,17 @@
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function subtract(num1, num2) {
+    return num1 - num2
+}
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function divide(num1, num2) {
+    return num1 / num2
+}
+
+console.log(add(3, 5), subtract(3, 5), multiply(3 ,5), divide(3, 5))
