433b1afda3ff93a3f066c39b8d6e6c86e8d4516a
mami <115097751+chorumlablabeasy@users.noreply.github.com>
2025-04-29 18:24:21 +0300

    select all buttons and add a event listener

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-let num1
-let num2
-let operator
-
 const numberButtons = document.querySelectorAll(".number")
 
-let displayElement = document.querySelector(".display")
+const displayElement = document.querySelector(".display")
+
+const operatorButtons = document.querySelectorAll(".operator")
+
+const resultButton = document.querySelector(".result")
+
+const clearButton = document.querySelector(".clear")
+
+let num1
+
+let num2
+
+let operator
 
 numberButtons.forEach(button => button.addEventListener("click", event => displayElement.textContent += event.target.textContent))
+
+operatorButtons.forEach(button => button.addEventListener("click", event => {
+    operator = event.target.textContent
+    num1 = Number(displayElement.textContent)
+    displayElement.textContent = ""
+}))
+
+resultButton.addEventListener("click", () => {
+    num2 = Number(displayElement.textContent)
+    displayElement.textContent = operate(operator, num1, num2)
+})
+
+clearButton.addEventListener("click", () => {
+    num1 = 0
+    num2 = 0
+    operator = ""
+    displayElement.textContent = ""
+})
 
 function add(num1, num2) {
     return num1 + num2
