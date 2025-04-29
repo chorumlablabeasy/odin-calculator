4c816e76aa754f93e669f84b99ac395885cdf345
mami <115097751+chorumlablabeasy@users.noreply.github.com>
2025-04-30 02:23:57 +0300

    fix all bugs and finish projects

--- script.js
+++ script.js
@@ -14,25 +14,39 @@ let num2
 
 let operator
 
-numberButtons.forEach(button => button.addEventListener("click", event => displayElement.textContent += event.target.textContent))
+let shouldReset = false
+
+numberButtons.forEach(button => button.addEventListener("click", event => {
+    if (shouldReset) {
+        resetCalculator()
+    }
+    displayElement.textContent += event.target.textContent
+    }))
 
 operatorButtons.forEach(button => button.addEventListener("click", event => {
+    if (displayElement.textContent === "") return
+    if (shouldReset) {
+            num1 = Number(displayElement.textContent)
+            shouldReset = false
+            displayElement.textContent = ""
+    } else if (num1 === undefined) {
+            num1 = Number(displayElement.textContent)
+            displayElement.textContent = ""
+    }
     operator = event.target.textContent
-    num1 = Number(displayElement.textContent)
-    displayElement.textContent = ""
-}))
+    }))
 
 resultButton.addEventListener("click", () => {
+    if (!operator || displayElement.textContent === "") return
     num2 = Number(displayElement.textContent)
     displayElement.textContent = operate(operator, num1, num2)
-})
+    num1 = Number(displayElement.textContent)
+    num2 = undefined
+    operator = undefined
+    shouldReset = true
+    })
 
-clearButton.addEventListener("click", () => {
-    num1 = 0
-    num2 = 0
-    operator = ""
-    displayElement.textContent = ""
-})
+clearButton.addEventListener("click", () => resetCalculator())
 
 function add(num1, num2) {
     return num1 + num2
@@ -47,6 +61,9 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+    if (num2 === 0) {
+        return "Nice Try :P"
+    }
     return num1 / num2
 }
 
@@ -60,4 +77,12 @@ function operate(operator, num1, num2) {
     } else if (operator === "/") {
         return divide(num1, num2)
     }
+}
+
+function resetCalculator() {
+    num1 = undefined
+    num2 = undefined
+    operator = undefined
+    shouldReset = false
+    displayElement.textContent = ""
 }
